7c89439f0b772ae77f4e1d3f62f7f24bdcabea0c
speraza545 <s.peraza545@gmail.com>
2021-04-15 13:39:58 -0400

    slight formatting to make thigns centered after adding the account button made them askew

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,12 +4,16 @@ class UserController < ApplicationController
     end
 
     post "/signup" do 
-        user = User.new(params)
-        if user.save
-            session[:user_id] = user.id
-            redirect "/jobs"
-        else
+        if !!User.find_by(email: params[:email])
             redirect "/signup"
+        else
+            user = User.new(params)
+            if user.save
+                session[:user_id] = user.id
+                redirect "/jobs"
+            else
+                redirect "/signup"
+            end
         end
     end
 
@@ -39,5 +43,64 @@ class UserController < ApplicationController
           redirect to '/'
         end
       end
+    get "/user/:id" do
+      @user = User.find(params[:id])
+      if logged_in? && @user.id == current_user.id
+      erb :'user/show'
+      else
+        redirect "/login"
+      end
+    end
+
+    get '/user/:id/edit' do
+        @user = User.find(params[:id])
+        if logged_in?
+        erb :"/user/edit"
+        else 
+            redirect "/login"
+        end
+    end
+
+    patch '/user/:id' do
+        @user = User.find(params[:id])
+        if !!User.find_by(email: params[:email]) && @user != User.find_by(email: params[:email])
+            redirect "/user/#{@user.id}/edit"
+        else
+            if logged_in? && @user.id = current_user.id
+                @user.update(name: params[:name], email: params[:email], phone_number: params[:phone_number], address: params[:address])
+                redirect "/user/#{@user.id}"
+            else
+                redirect "/login"
+            end
+        end
+    end
+
+    delete "/user/:id/delete" do
+            user = User.find(params[:id])
+            jobs = Job.where(user_id: user.id)
+            if logged_in? && user.id == current_user.id
+                if jobs !=nil
+                    jobs.each do |job|
+                        if job.rooms != nil
+                            job.rooms.each do |room|
+                                room.delete
+                            end
+                            job.delete
+                        else
+                            job.delete 
+                        end
+                    end
+                    user.delete
+                    session.destroy
+                    redirect "/"
+                else
+                    user.delete
+                    session.destroy
+                    redirect "/"
+                end
+            else
+                redirect "/login"
+            end
+        end
 
 end
